1ebf8d76f79f994dd5116c8ebd069c7b691b8d6c
Level8Broccoli <Level8Broccoli@users.noreply.github.com>
2021-08-24 20:48:32 +0000

    deploy: gilde-der-nacht/website@81e0a82b4f9cf1e60c4c8ae461c86cc427900991

--- scripts/caerus.react.js
+++ scripts/caerus.react.js
@@ -18,10 +18,12 @@ class ProgramDay extends React.Component {
         Object.entries(this.props.state).forEach(([hour, details]) => {
             const lastEntry = list.length ? list[list.length - 1] : { details: { game_id: undefined } };
 
-            if (hour === "12") {
-                list.push({ hour, details: { title: "Mittagspause" }, duration: 1 })
+            if (hour === "12" && this.props.title.startsWith("Samstag")) {
+                list.push({ hour, details: { title: "Mittagessen (Kiosk verfügbar)" }, duration: 1 })
+            } else if (hour === "12") {
+                list.push({ hour, details: { title: "Mittagessen (warme Küche mit Menü verfügbar)" }, duration: 1 })
             } else if (hour === "19") {
-                list.push({ hour, details: { title: "Znachtpause" }, duration: 1 })
+                list.push({ hour, details: { title: "Abendessen (warme Küche mit Menü verfügbar)" }, duration: 1 })
             } else if (details === null) {
                 if (lastEntry.details === null) {
                     lastEntry.duration += 1;
@@ -59,7 +61,7 @@ class ProgramDay extends React.Component {
                         )
                     }
 
-                    if (details.title === "Mittagspause" || details.title === "Znachtpause") {
+                    if (details.title.startsWith("Mittagessen") || details.title.startsWith("Abendessen")) {
                         return e("li", { key: hour, className: "c-caerus-entry c-caerus-eat c-caerus-duration-" + duration },
                             e("span", null, hour + " Uhr"),
                             e("span", null, details.title)
